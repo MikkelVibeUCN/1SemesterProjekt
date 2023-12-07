5b594194159fda42e0e731884e2f655666f490b1
Mikkel <10503091@ucn.dk>
2023-12-07 13:29:57 +0100

    Added calculatePrice method

--- 1SemesterProjekt/src/model/Discount.java
+++ 1SemesterProjekt/src/model/Discount.java
@@ -3,6 +3,7 @@ package model;
 public class Discount {
 	private String type;
 	private static int count;
+	private double discount;
 	private int discountID;
 	private float discountPercentage;
 	private float loanDiscountPercentage;
@@ -10,5 +11,24 @@ public class Discount {
 	public Discount(String type) {
 		this.type = type;
 		discountID = ++count;
+	}
+	
+	public void setDiscount(double discount) {
+		this.discount = discount;
+	}
+	
+	public void setDiscountPercentage(float discountPercentage) {
+		this.discountPercentage = discountPercentage;
+	}
+	
+	public double calculatePrice(double amount) {
+		double result = -1;
+		if(discountPercentage != 0) {
+			result = amount*discountPercentage;
+		}
+		else if (discount != 0) {
+			result = amount - discount;
+		}
+		return result;
 	}
 }
