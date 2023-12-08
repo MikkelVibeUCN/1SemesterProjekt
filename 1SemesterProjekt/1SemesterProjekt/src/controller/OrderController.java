3182490ca1592fdc7537ae1ece57425cf7e8a4b8
MandeMads <10201992@ucn.dk>
2023-12-08 11:25:12 +0100

    Controllers

--- 1SemesterProjekt/1SemesterProjekt/src/controller/OrderController.java
+++ 1SemesterProjekt/1SemesterProjekt/src/controller/OrderController.java
@@ -1,5 +1,20 @@
 package controller;
+import javax.swing.plaf.basic.BasicBorders;
+
+import model.*;
+import model.order.*;
+import model.product.*;
 
 public class OrderController {
-
+	private OrderContainer orderContainer;
+	private SalesAssistantController salesAssistantController;
+	
+	// Create a new order with salesassistant from controller
+	public Order createOrder(int employeeID) {
+		return new Order(salesAssistantController.findSalesAssistant(employeeID));
+	}
+	
+	/*public boolean addCustomerToOrder(String phoneNo, Order order) {
+		
+	} */
 }
