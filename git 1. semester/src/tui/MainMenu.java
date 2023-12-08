ad885d05f7a5145782e7bab12800cb0e62ccc3df
Andershsm <10503096@ucn.dk>
2023-12-08 10:01:16 +0100

    added MainMenu, OrderMenu

--- git 1. semester/src/tui/MainMenu.java
+++ git 1. semester/src/tui/MainMenu.java
@@ -1,10 +1,49 @@
-package tui;
+import java.util.Scanner;
 
 public class MainMenu {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
 
-	}
+        while (!exit) {
+            
+            System.out.println("1. Ordre menu");
+            System.out.println("2. ");
+            System.out.println("3. ");
+            System.out.println("0. Afslut");
 
+   
+            int choice = scanner.nextInt();
+
+          
+            switch (choice) {
+                case 1:
+                	//
+                 
+                    break;
+                case 2:
+                	//
+            
+                    break;
+                case 3:
+                    //
+              
+                    break;
+                case 4:
+                    exit = true;
+               
+                    break;
+                default:
+                    System.out.println("Ugyldigt valg");
+            }
+        }
+
+        scanner.close();
+    }
+
+    public static void main(String[] args) {
+        MainMenu menu = new MainMenu();
+        menu.run();
+    }
 }
--- git 1. semester/src/tui/OrderMenu.java
+++ git 1. semester/src/tui/OrderMenu.java
@@ -1,5 +1,53 @@
-package tui;
+import java.util.Scanner;
+
+import controller.OrderController;
 
 public class OrderMenu {
+    private OrderController orderController;
+    private Scanner scanner;
 
+    public OrderMenu() {
+        this.orderController = new OrderController();
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void displayMenu() {
+        System.out.println("Ordre Menu:");
+        System.out.println("1. Create Order");
+        System.out.println("0. Gå tilbage");
+
+        System.out.print("Choose an option: ");
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                // createOrder();
+                break;
+            case 2:
+                // 
+                break;
+            case 3:
+                break;
+            case 0:
+                // menu.run();
+                break;
+            default:
+                System.out.println("Ugyldigt valg");
+        }
+    }
+    public void createOrder(int employeeID) {
+    	//createOrder();
+    }
+    public void addCustomerToOrder(String phoneNo, Order order) {
+    	//addCustomerToOrder();
+    }
+    public void addProductByBarcode(int Barcode) {
+    	//addProductToOrder();
+    }
+    public void addProductBySerialNo(int serialNo) {
+    	//addProductBySerialNo();
+    }
+    public void confirmOrder(Order order) {
+    	//confirmOrder();
+    }
 }
