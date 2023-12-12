02da811f99e7480882df8f4cae2ecf9bf27fe3b3
Andershsm <10503096@ucn.dk>
2023-12-12 10:13:09 +0100

    composite

--- 1SemesterProjekt/1SemesterProjekt/src/model/product/Composite.java
+++ 1SemesterProjekt/1SemesterProjekt/src/model/product/Composite.java
@@ -1,5 +1,37 @@
 package model.product;
 
-public class Composite {
+import java.util.ArrayList;
 
+public class Composite extends Product {
+	private String SKU;
+	private int barcode;
+	private int minimumStock;
+	private int maximumStock;
+	private int stockAmount;
+	private String productGroup;
+	private ArrayList<Product> products;
+	
+	public Composite (String name, double minimumPrice, double price, String SKU, int barcode, int minimumStock, int maximumStock, int stockAmount, String productGroup){
+		super(name,minimumPrice,price,barcode);
+		this.SKU = SKU;
+		this.barcode = barcode;
+		this.minimumStock = minimumStock;
+		this.maximumStock = maximumStock;
+		this.stockAmount = stockAmount;
+		this.productGroup = productGroup;
+		this.products = new ArrayList<>();
+		
+	}
+	
+	public void addProduct(Product product) {
+		products.add(product);
+	}
+	
+	public double getPrice() {
+		double totalPrice = 0;
+		for(Product product : products) {
+			totalPrice += product.getPrice();
+		}
+		return totalPrice;
+	}
 }
--- 1SemesterProjekt/1SemesterProjekt/src/model/product/ShelfProduct.java
+++ 1SemesterProjekt/1SemesterProjekt/src/model/product/ShelfProduct.java
@@ -18,5 +18,6 @@ public class ShelfProduct extends Product{
 		this.productGroup = productGroup;
 		
 	}
+	
 }
 
